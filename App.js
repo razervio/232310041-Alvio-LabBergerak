551f790dd4c5c9201647cf3dda7f3bd431a43d09
Alvio <alvio@example.com>
2025-04-17 07:54:18 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MyFriend from './src/components/friends/widgets/MyFriend';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Welcome to the App!</Text> */}
+      <MyFriend/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:0,
   },
 });
